a5884887f2a9fbce86114d32c05c466954d199f2
abhibhrt <adjack720@gmail.com>
2026-01-15 03:57:14 +0530

    fixed error

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -4,27 +4,29 @@ import Project from '../../models/project.model';
 import { deleteFromCloud } from '../../lib/cloudinary';
 
 export async function DELETE(
-    req: NextRequest,
-    { params }: { params: { id: string } }
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
-    await connectDB();
-    
-    const { id } = await params;
+  await connectDB();
 
-    try {
-        const project = await Project.findById(id);
+  const { id } = await params;
 
-        if (!project) return NextResponse.json({ error: 'Not found' }, { status: 404 });
+  try {
+    const project = await Project.findById(id);
 
-        if (project.public_id) {
-            await deleteFromCloud(project.public_id);
-        }
-
-        await Project.findByIdAndDelete(id);
-
-        return NextResponse.json({ message: 'Project purged' }, { status: 200 });
-
-    } catch (error) {
-        return NextResponse.json({ error: 'Delete failed' }, { status: 500 });
+    if (!project) {
+      return NextResponse.json({ error: 'Not found' }, { status: 404 });
     }
+
+    if (project.public_id) {
+      await deleteFromCloud(project.public_id);
+    }
+
+    await Project.findByIdAndDelete(id);
+
+    return NextResponse.json({ message: 'Project purged' }, { status: 200 });
+
+  } catch (error) {
+    return NextResponse.json({ error: 'Delete failed' }, { status: 500 });
+  }
 }
